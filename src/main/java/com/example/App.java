477b90e863938435fc2034476117e539e9a19071
aksajiji <aksajiji354@gmail.com>
2026-05-02 10:00:45 +0530

    Task 3 Devops

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Hello world! This is Aksa
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is Aksa
+" );
+    }
+}
